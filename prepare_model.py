cc68196802786aa3a769745d9e13c408a8c4821a
yakov <golovanev98@mail.ru>
2019-08-18 22:42:53 +0300

    Improve model quality by training on better non-claps data and using batch normalization

--- prepare_model.py
+++ prepare_model.py
@@ -12,7 +12,7 @@ claps_dir = data_base_path + 'claps/'
 non_claps_dir = data_base_path + 'non-claps/'
 all_features_file = data_base_path + 'all_features.npy'
 all_labels_file = data_base_path + 'all_labels.npy'
-count_of_files_to_extract_features = 100
+count_of_files_to_extract_features = 1500
 
 
 def get_features_and_labels():
@@ -26,7 +26,7 @@ def get_features_and_labels():
     return all_features, all_labels
 
 
-def process_data_directory(directory):
+def process_data_directory(directory, extractor):
     features = []
     labels = []
     for filename in listdir(directory)[:count_of_files_to_extract_features]:
@@ -42,19 +42,21 @@ def process_data_directory(directory):
 
 
 def extract_features_from_file():
-    clap_features, clap_labels = process_data_directory(claps_dir)
-    non_clap_features, non_clap_labels = process_data_directory(non_claps_dir)
+    clap_features, clap_labels = process_data_directory(claps_dir, claps_extractor)
+    non_clap_features, non_clap_labels = process_data_directory(non_claps_dir, non_claps_extractor)
     one_class_sample_count = min(len(clap_features), len(non_clap_features))
     return np.array(clap_features[:one_class_sample_count] + non_clap_features[:one_class_sample_count]), \
            np.array(clap_labels[:one_class_sample_count] + non_clap_labels[:one_class_sample_count])
 
 
 pattern_filenames = ['data/pattern-1.npy', 'data/pattern-2.npy']
-extractor = FeatureExtractor(feature_count=10, distance_factor=10, pattern_files=pattern_filenames,
-                             segment_duration=1024, feature_size=64)
-wrapper = KerasModelWrapper(input_length=extractor.feature_size)
+claps_extractor = FeatureExtractor(feature_count=5, distance_factor=10, pattern_files=pattern_filenames,
+                                   segment_duration=1024, feature_size=127)
+non_claps_extractor = FeatureExtractor(feature_count=1, distance_factor=10, pattern_files=pattern_filenames,
+                                       segment_duration=1024, feature_size=127)
+wrapper = KerasModelWrapper(input_length=claps_extractor.feature_size)
 all_features, all_labels = get_features_and_labels()
 x_train, x_test, y_train, y_test = train_test_split(all_features, all_labels, test_size=0.3)
-history = wrapper.fit_model(x_train, x_test, y_train, y_test, extractor.feature_size)
+history = wrapper.fit_model(x_train, x_test, y_train, y_test, claps_extractor.feature_size, epochs=30)
 wrapper.save_model()
 wrapper.show_learning_curve(history)
